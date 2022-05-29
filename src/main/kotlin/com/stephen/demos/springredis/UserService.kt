9aba00492969c847839a627455eb406b48dc0730
Stephen Mafole <stephen.mafole@elikya.tech>
2022-05-29 19:40:18 +0100

    implemented findAll

--- src/main/kotlin/com/stephen/demos/springredis/UserService.kt
+++ src/main/kotlin/com/stephen/demos/springredis/UserService.kt
@@ -3,4 +3,9 @@ package com.stephen.demos.springredis
 class UserService(
     private val userRepository: UserRepository
 ) {
+
+    fun findAll(): List<User> {
+        val users = userRepository.findAll()
+        return users.toList()
+    }
 }
